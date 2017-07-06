a116b7efa67f5cd082f13b7edb3df4cecbb688d7
thedreamsaver <ashishjhaofficial@outlook.com>
2017-07-06 20:20:13 +0530

    Added post form

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
-  
+  resources :posts
   root 'pages#index'
 
   get 'home' => 'pages#home'
 
-  get 'profile/:id' => 'pages#profile'
+  get 'user/:id' => 'pages#profile'
 
   get 'explore' => 'pages#explore'
 
